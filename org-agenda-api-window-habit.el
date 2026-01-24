e539d5f793f63baf2bea5d0ac627947e82709983
Ivan Malison <IvanMalison@gmail.com>
2026-01-24 13:52:06 -0800

    Revert "refactor: use org-window-habit-entry-p for habit detection"
    This reverts commit 02db427179f8b66190d45513df76f66fa02450b0.

--- org-agenda-api-window-habit.el
+++ org-agenda-api-window-habit.el
@@ -32,9 +32,11 @@
   (defun org-agenda-api--is-window-habit-p ()
     "Return non-nil if entry at point is an org-window-habit.
 Must be called with point at an org heading.
-Delegates to `org-window-habit-entry-p' from the org-window-habit library."
+Checks for both new format (WINDOW_SPECS) and old format (WINDOW_DURATION)."
     (and (org-agenda-api--window-habit-available-p)
-         (org-window-habit-entry-p)))
+         (or (org-entry-get nil (org-window-habit-property "ASSESSMENT_INTERVAL") t)
+             (org-entry-get nil (org-window-habit-property "WINDOW_SPECS") t)
+             (org-entry-get nil (org-window-habit-property "WINDOW_DURATION") t))))
 
   (defun org-agenda-api--get-habit-summary ()
     "Get habit summary for the entry at point.
