4b9fc0abf05fb5698898d97e565870be6f3e8aa6
Ivan Malison <IvanMalison@gmail.com>
2026-08-20 00:19:37 -0700

    chore: bump to org-agenda-api 4.5.1

--- org-agenda-api.el
+++ org-agenda-api.el
@@ -4,7 +4,7 @@
 
 ;; Author: Ivan Malison <IvanMalison@gmail.com>
 ;; URL: https://github.com/IvanMalison/org-agenda-api
-;; Version: 4.5.0
+;; Version: 4.5.1
 ;; Package-Requires: ((emacs "26.1") (simple-httpd "1.5.1"))
 ;; Keywords: org, agenda, api, json
 
